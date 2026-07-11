9c41c2c551835fb469057d883b3da80a5421ecf7
Harveen Kaur <harveendua@gmail.com>
2026-07-11 10:45:15 +0530

    power bi

--- pages/pages/powerbi_dashboard.py
+++ pages/pages/powerbi_dashboard.py
@@ -13,7 +13,7 @@ st.markdown(
     "View the interactive Power BI analytics dashboard below."
 )
 
-POWERBI_URL = "https://app.powerbi.com/groups/me/reports/d772f056-8e72-44f2-9e5a-858f13b2751f/47e7676745b7fa6e3b3c?experience=power-bi"
+POWERBI_URL = "https://app.powerbi.com/groups/me/reports/d772f056-8e72-44f2-9e5a-858f13b2751f/7f7e694044448ee16bd4?experience=power-bi"
 
 components.iframe(
     POWERBI_URL,
